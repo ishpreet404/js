b4cd4ac1a9b514fac849d85b5aa4aee17b1bd962
Ishpreet Singh <ishpreet@outlook.in>
2025-02-23 19:02:05 +0530

    initial

---
+++ 01.js
@@ -0,0 +1,19 @@
+// // // getname();
+// // console.log(getname);
+// // console.log(x); //hoisting 
+// // var x =7;
+// // function getname(){
+// //     console.log("Heloo")
+// //     //functions gets printed without declaring it 
+// // }
+// var x = 1;
+// a();//seperate execution context will beb created , mem will be allocated to all var in the function 
+// b();
+// console.log(x);
+// function a(){
+//     var x =99;
+//     console.log(x);
+// }
+// function b(){
+//     console.log("bhalu");
+// }
