959b1a04594f6fa7c58acfb0c4b863bcda10dde5
Ishpreet Singh <ishpreet@outlook.in>
2025-02-27 18:21:14 +0530

    firstclassfunction

---
+++ firstclassfunction.js
@@ -0,0 +1,23 @@
+// a();
+// b();//error
+//funtion statement or function declaration 
+function a(){
+    console.log("fn called")
+}
+//function expression 
+var b = function (param1){//paramter //hoisting is not done until interpretor reaches the line 
+    console.log("b called")
+}
+a();
+b(1); //arg
+//anonymous function
+// function (){
+// }
+// named function expression
+var c = function xyz (){
+    console.log("c called");
+}
+c();
+// xyz(); //error
+
+//firstclassfunction
