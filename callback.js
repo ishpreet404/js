0fb4f76a170c3c4c888e43c61489eab25aa619b9
Ishpreet Singh <ishpreet@outlook.in>
2025-02-28 22:11:40 +0530

    await

--- callback.js
+++ callback.js
@@ -1,11 +1,14 @@
-setTimeout(function (){
-    console.log("timer");
+// setTimeout(function (){
+//     console.log("timer");
     
-},5000)
-function x(y) {
-    console.log("x");
-    y();
-}
-x(function y(){ //y is  passed into x 
-    console.log("y");
+// },5000)
+// function x(y) {
+//     console.log("x");
+//     y();
+// }
+// x(function y(){ //y is  passed into x 
+//     console.log("y");
+// });
+document.getElementById("hello").addEventListener("click",function xyz(){
+    console.log("button daba diya");
 });
