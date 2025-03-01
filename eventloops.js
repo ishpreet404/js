98ec0d5d8216c886d02c53a4ef10a000e1eeeb99
Ishpreet Singh <ishpreet@outlook.in>
2025-03-01 19:49:33 +0530

    timeout cb stack

--- eventloops.js
+++ eventloops.js
@@ -1,1 +1,13 @@
 //all comebacks from promises and mutation observer comes into microtask queue
+console.log("start");
+setTimeout(function cb(){
+    console.log("callback");
+},0);
+console.log("end");
+let startdate = new Date().getTime();
+let enddate = startdate;
+while(enddate < startdate + 5000){
+    enddate=new Date().getTime();
+}
+console.log("while ended");
+//settimout ensures the minimum delay not the total dealy 
