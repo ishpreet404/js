115bc93e342b06b3e3b7748bad9ffb5ba4c3e77a
Ishpreet Singh <ishpreet@outlook.in>
2025-03-04 16:08:53 +0530

    promises

--- promises.js
+++ promises.js
@@ -1,5 +1,9 @@
-//callback hell
-const cart =["shoes","pants"];
-api.createorder(cart,function(){
-    api.createpayment()
+// //callback hell
+const cart =["shoes","pants","chapal"];
+// api.createorder(cart,function(){
+//     api.createpayment()
+// });
+const promise = createorder(cart); // as soon as this gets the value .then function is called
+promise.then(function(orderid){ //will be called once regardless of number of times api calls the create order function
+    proceedtopayemnt(orderid);
 });
