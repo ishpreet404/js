b5eb504c705761d1da1557b338eee2e921f941da
Ishpreet Singh <ishpreet@outlook.in>
2025-03-06 19:20:05 +0530

    promise_apis

---
+++ promise_apis.js
@@ -0,0 +1,4 @@
+//promise.all() if one promise fails then result will give error
+// promise.allsetteled() if one of the promises get rejected it will wait for all promises to settle it will give all the results 
+// promise.race() the first promise resolved will be returned if the first promise fails error will be thrown
+//promise,any() whenever the first promise gets reolved (success) it returns the promise
