b8efe713cd0fb79a2a6930c872f5365e23c2e284
Ishpreet Singh <ishpreet@outlook.in>
2025-03-06 19:37:24 +0530

    aggregate error and promise.any

--- promise_apis.js
+++ promise_apis.js
@@ -3,19 +3,26 @@
 // promise.race() the first promise resolved will be returned if the first promise fails error will be thrown
 //promise,any() whenever the first promise gets reolved (success) it returns the promise if all failes then it will return a aggregated error (array of errors)
 const p1 =new Promise((resolve,reject)=>{
-    setTimeout(()=> resolve("p1 success"),3000);
+    setTimeout(()=> reject("p1 fail"),3000);
 })
 const p2 =new Promise((resolve,reject)=>{
     // setTimeout(()=> resolve("p1 success"),1000);
-    setTimeout(()=> reject("p1 fail"),1000);
+    setTimeout(()=> reject("p2 fail"),1000);
 })
 const p3 =new Promise((resolve,reject)=>{
-    setTimeout(()=> resolve("p1 success"),4000);
+    setTimeout(()=> reject("p3 fail"),4000);
 })
 
 // Promise.all([p1,p2,p3]).then((res)=>{
 //     console.log(res);
 // }).catch((err) => console.error(err));
-Promise.allSettled([p1,p2,p3]).then((res)=>{
-    console.log(res);
-}).catch((err) => console.error(err));
+// Promise.race([p1,p2,p3]).then((res)=>{
+//     console.log(res);
+// }).catch((err) => console.error(err));
+// Promise.allSettled([p1,p2,p3]).then((res)=>{
+    //     console.log(res);
+    // }).catch((err) => console.error(err));
+Promise.any([p1,p2,p3]).then((res)=>{   // if all fails it gives a aggregate error
+        console.log(res);  
+}).catch((err) => console.error(err.errors));
+ //settled => resolve (fullfilled)  || reject (failure)   
