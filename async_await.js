51550a8a336ac32572a48e053371d7078c026353
Ishpreet Singh <ishpreet@outlook.in>
2025-03-07 23:04:17 +0530

    async_await

--- async_await.js
+++ async_await.js
@@ -8,10 +8,24 @@
 
 // //
 const p = new Promise((resolve,reject)=>{
-    resolve("promise resolved value");
+    setTimeout(()=>{
+        resolve("promise resolved value");
+
+    },2000)
+})
+const p2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("promise resolved value");
+
+    },4000)
 })
 async function handlepromise() {
-    const val = await p;
+    const val = await p;  //await can only be used inside a async function
+    console.log("this line will be exectued after promise is resolved");
     console.log(val);
+    const val2 = await p2;  //await can only be used inside a async function
+    console.log("this ");
+    console.log(val2);
 }
-handlepromise();
+handlepromise();//function exectution suspesnds when it sees await function until promise is reolved 
+
